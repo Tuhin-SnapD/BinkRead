8db1739e28c0060f12f54e7b3e2838dfdbde4921
Ayushi-Rahane <ayushirahane2021@gmail.com>
2025-06-24 02:38:00 +0530

    integrated summary feature

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from transformers import pipeline
+import fitz
+import torch
+
+print("Torch version:", torch.__version__)
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#Uploading pdf into database function
+@app.route('/upload', methods=['POST'])
+def upload_pdf():
+   file = request.files['uploaded_file']
+   if file and file.filename.endswith('.pdf'):
+       pdf = fitz.open(stream=file.read(), filetype="pdf")
+       pdf_text = ""
+       for page in pdf:
+           pdf_text += page.get_text()
+       summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+       summary = summarizer(pdf_text, max_length=100, min_length=30)
+       print(summary)
+       return render_template("index.html",summary=summary)
+   return "Error"
+       
+       
+       
+       
+       
+
+if __name__ == '__main__':
+    app.run(debug=True)
